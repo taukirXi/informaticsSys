3c81b0953d06264d83fecc3b5a4de5d7f1459879
TaukirAhmed <33090525+taukirXi@users.noreply.github.com>
2024-10-22 12:55:37 +0100

    initial commit

---
+++ informationSys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for informationSys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'informationSys.settings')
+
+application = get_asgi_application()
