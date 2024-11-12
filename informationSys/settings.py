ce42f320b039ff37a3a443851a7c7ee156f4b466
TaukirAhmed <33090525+taukirXi@users.noreply.github.com>
2024-11-12 10:09:28 +0000

    initial setups complete

--- informationSys/settings.py
+++ informationSys/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'informationSys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'infosys',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
